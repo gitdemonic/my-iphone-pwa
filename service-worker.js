3c54bd91612b8b3a8f79953d447ac44520e986e9
gitdemonic <mydemonic@gmail.com>
2025-12-30 23:25:47 +0800

    update: add offline support and UI polish

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'trip-pwa-v2';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html'
+];
+
+// 安裝時快取檔案
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// 啟動時清理舊快取
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 攔截請求：先看快取，沒快取才去抓網路
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
